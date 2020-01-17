6906e1649a1fc8857326e26e914e2282807afebe
Hiago-Patrício <hiagodesousa13@gmail.com>
2020-01-17 17:21:31 -0400

    projeto configurado para implementar cdi e taglib

--- build/generated-sources/ap-source-output/entidades/Autor_.java
+++ build/generated-sources/ap-source-output/entidades/Autor_.java
@@ -5,7 +5,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2019-11-17T15:38:19")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2020-01-17T18:16:39")
 @StaticMetamodel(Autor.class)
 public class Autor_ { 
 
